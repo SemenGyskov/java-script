1338ea1a9ac73e92da7fb3f01de267b24959a506
SemenGyskov <114874984+SemenGyskov@users.noreply.github.com>
2022-10-29 04:47:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,176 @@
+// 1
+// alert('Я Java Script!');
+// console.log('Я Java Script!');
+
+
+
+// 2
+// let name = prompt("Ваше Имя","Саймон");
+// alert(`Ваше Имя ${name}`);
+
+
+
+
+// 3
+
+// let a = 1, b =1;
+// let c = ++a; // ?
+// let d = b++; // ?
+// alert(a)
+// alert(b)
+// alert(c)
+// alert(d)
+
+
+
+// 4
+// let a = 2;
+// let x = 1 + (a *= 2);
+// alert(a)
+// alert(x)
+
+
+// 5
+
+// console.log("" + 1 + 0);
+// console.log("" - 1 + 0);
+// console.log(true + false);
+// console.log(6 / "3");
+// console.log("2" * "3");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 +5);
+// console.log("4" - 2);
+// console.log("4px" - 2);
+// console.log(" -9 " + 5);
+// console.log(" -9 " - 5);
+// console.log(null + 1);
+// console.log(undefined +1);
+// console.log(" \t \n" -2);
+
+
+// 6
+// let a = prompt("Первое число", 1);
+// let b = prompt("Второе число", 2);
+// console.log(Number(a) + Number(b));
+
+
+// 1 Работа с переменными
+
+// let num = 3;
+// alert(num);
+
+// 2
+
+// let a = 10;
+// let b = 2;
+// alert(Number(a) + Number(b));
+// alert(Number(a) - Number(b));
+// alert(Number(a) * Number(b));
+// alert(Number(a) / Number(b));
+
+// 3
+
+// let a = 15;
+// let d = 2;
+// let result = a + d;
+// alert(result);
+
+// 4
+
+// let a = 10;
+// let b = 2;
+// let c = 5;
+// alert(a + b + c);
+
+// 5
+
+// let a = 17;
+// let b = 10;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+// alert(result);
+
+
+
+
+// 1 работа со строками
+
+// let str = 'Привет Мир!';
+// alert(str);
+
+// 2
+
+// let str1 = 'Привет, ';
+// let str2 = 'Мир!';
+// alert(str1 + str2);
+
+// 3
+
+// let name = 'Семён';
+// alert(`Привет, ${name}`);
+
+// 4
+
+// let age = '16';
+// alert(`Мне ${age} лет!`);
+
+
+
+// 1 Функция prompt
+
+// let name = prompt('Какое ваше имя?');
+// alert(`Ваше имя ${name}`);
+
+// 2
+
+// let number = prompt('Введите число');
+// alert(number**2);
+
+
+// 1 обращение к символам строки
+
+// let str = 'abcde';
+// alert(str[0]);
+// alert(str[2]);
+// alert(str[4]);
+
+// 2
+
+// let num = '12345';
+// alert(Number(num[0]) * Number(num[1]) * Number(num[2]) * Number(num[3]) * Number(num[4]));
+
+
+
+//1 Практика
+
+// alert(60 * 60 * 24 * 28)
+
+// 2
+
+// let час = '4 часа';
+// let минута = ' 32 минуты';
+// let секунда = ' 5 секунд';
+// alert(час+минута+секунда)
+
+// 3
+
+// let num = prompt('Введите число');
+// alert(num**2);
+
+// 1 Работа с присваиванием и декрементами
+
+// let num = 47;
+// num = num += 7;
+// num = num -= 18;
+// num = num *= 10;
+// num = num /= 15;
+// alert(num);
+
+// 2
+
+// var num = 10;
+// num = num ++ ;1;
+// num = num ++ ;1;
+// num = num -- ;1;
+// alert(num);
